9025f411a9211d5ace5f5e91f75434c8577b603a
ElMargaritox <alexisguzman0303@gmail.com>
2022-02-12 15:55:32 -0300

    Add project files.

---
+++ VPNFinder/Program.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPNFinder
+{
+    internal class Program
+    {
+        public static string Ip { get; set; }
+        static void Main(string[] args)
+        {
+            WebClient webclient = new WebClient();
+            while (true)
+            {
+                Console.Write("ESCRIBE LA DIRECCION IP: ");
+                string ip = Console.ReadLine();
+                string data = webclient.DownloadString(string.Format("https://proxycheck.io/v2/{0}?vpn=1", ip));
+                Ip = ip;
+                try
+                {
+                    var desA = Deserialize<Root>(data, true);
+                    Console.WriteLine(desA._1855423043.type.ToString());
+                }
+                catch { }
+                
+            }
+            
+
+
+        }
+
+        // Serialize Json
+        static string Serialize(object obj, bool newNames)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.Formatting = Formatting.Indented;
+            if (newNames)
+            {
+                settings.ContractResolver = new CustomNamesContractResolver();
+            }
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+
+        // Deserialize Json
+        static T Deserialize<T>(string text, bool newNames)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.Formatting = Formatting.Indented;
+            if (newNames)
+            {
+                settings.ContractResolver = new CustomNamesContractResolver();
+            }
+
+            return JsonConvert.DeserializeObject<T>(text, settings);
+        }
+    }
+    class CustomNamesContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(System.Type type, MemberSerialization memberSerialization)
+        {
+            // Let the base class create all the JsonProperties 
+            // using the short names
+            IList<JsonProperty> list = base.CreateProperties(type, memberSerialization);
+
+            // Now inspect each property and replace the 
+            // short name with the real property name
+            foreach (JsonProperty prop in list)
+            {
+                if (prop.UnderlyingName == "_1855423043") //change this to your implementation!
+                    prop.PropertyName = Program.Ip;
+
+            }
+
+            return list;
+        }
+    }
+
+    public class Customer
+    {
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        public string Test { get; set; }
+
+    }
+}
+
---
+++ VPNFinder/Root.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPNFinder
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Policies
+    {
+        public string ad_filtering { get; set; }
+        public string free_access { get; set; }
+        public string paid_access { get; set; }
+        public string port_forwarding { get; set; }
+        public string logging { get; set; }
+        public string anonymous_payments { get; set; }
+        public string crypto_payments { get; set; }
+        public string traceable_ownership { get; set; }
+    }
+
+    public class Operator
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string anonymity { get; set; }
+        public string popularity { get; set; }
+        public List<string> protocols { get; set; }
+        public Policies policies { get; set; }
+    }
+
+    public class _1855423043
+    {
+        public string proxy { get; set; }
+        public string type { get; set; }
+        public Operator @operator { get; set; }
+    }
+
+    public class Root
+    {
+
+        public string status { get; set; }
+
+        [JsonProperty(PropertyName = "_1855423043")]
+        public _1855423043 _1855423043 { get; set; }
+    }
+}
+
